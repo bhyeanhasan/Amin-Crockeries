c4c7e4ff8535f2cfc6fa81f77569748b627a5b24
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-09-25 18:03:08 +0600

    product search & category

--- manage_product/urls.py
+++ manage_product/urls.py
@@ -7,13 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('', views.product_list, name='home'),
                   path('details/<id>/', views.details, name='details'),
-                  path('category/<tag>/', views.category, name='category'),
-                  path('addtocard/<id>/', views.addToCard, name='addToCard'),
-                  path('deleteallcart/<id>/', views.deleteallcart, name='deleteallcart'),
-                  path('deletefromcart/<id>/', views.deletefromcart, name='deletefromcart'),
-                  path('addtowish/<id>/', views.addToWish, name='addToWish'),
-                  path('showWish/', views.showWish, name='showWish'),
-                  path('search_result/', views.search_result, name='search_result'),
-                  path('about/', views.about, name='about'),
-                  path('cart/', views.cart, name='cart'),
+                  path('category/<id>/', views.category, name='category'),
+                  path('searchresult/', views.search_result, name='search_result'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
